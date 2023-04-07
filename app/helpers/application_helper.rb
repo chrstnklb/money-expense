2dff4e481f489e7d13309f841bea562c975a72a0
Christian <chrstnklb@gmail.com>
2023-04-07 10:51:05 +0200

    fix design flaws

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,19 @@
 module ApplicationHelper
 
     def back_to_expenses_button
-        button_to "Zurück zu Ausgaben", money_expenses_path, method: :get, class: "btn btn-small btn-secondary"
+        button_to "🔙", money_expenses_path, method: :get, class: "btn btn-small btn-secondary"
     end       
 
     def delete_expense_button(expense)
-        button_to "Lösche diese Ausgabe", expense, method: :delete, data: { confirm: "Sind Sie sicher?" }, class: "btn btn-small btn-danger"
+        button_to "🗑", expense, method: :delete, data: { confirm: "Sind Sie sicher?" }, class: "btn btn-small btn-danger"
+    end
+
+    def edit_expense_button(expense)
+        button_to "⚙️", edit_money_expense_path(expense), method: :get, class: "btn btn-small btn-primary"
+    end
+    
+    def show_expense_button(expense)
+        button_to "🔍", expense, method: :get, class: "contrast outline btn-small btn-secondary"
     end
 
 end
