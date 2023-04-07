5d93d4b6827c9d6511f4c57d741c0cd2dd6031d5
Christian <chrstnklb@gmail.com>
2023-04-07 08:25:39 +0200

    postgres

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def back_to_expenses_button
+        button_to "Zurück zu Ausgaben", money_expenses_path, method: :get, class: "btn btn-small btn-secondary"
+    end       
+
+    def delete_expense_button(expense)
+        button_to "Lösche diese Ausgabe", expense, method: :delete, data: { confirm: "Sind Sie sicher?" }, class: "btn btn-small btn-danger"
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_04_230552) do
-  create_table "money_expenses", force: :cascade do |t|
-    t.integer "price", limit: 1
-    t.text "category"
-    t.text "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema[7.0].define(version: 0) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
 end
